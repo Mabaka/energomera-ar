cfecdbc5a34f1b193d915e8f560ae8de496ba611
KozlovNS <mabakainu@gmail.com>
2023-10-06 01:56:01 +0300

    init backend

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app: express.Application = express()
+
+app.get('/', (request: express.Request, response: express.Response) => {
+  response.send('Hello world')
+})
+
+app.listen(8080, () => {
+  console.log('Listening 8080')
+})
