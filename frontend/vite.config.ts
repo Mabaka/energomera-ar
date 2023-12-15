e1981fc69e95217fe5866926d7addfe40222b04d
Nikita Kozlov <kns@kns_pc>
2023-12-15 15:40:40 +0300

    all

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import fs from 'fs'
 import mkcert from 'vite-plugin-mkcert'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),mkcert()],
+  plugins: [vue()],
   resolve:{
     alias:{
       "@": fileURLToPath(new URL("./src", import.meta.url)),
