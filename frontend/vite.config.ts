1cd1fe74857539e5b0ada5ca24bd5e3e213ef240
Nikita Kozlov <kns@kns_pc>
2023-12-15 15:11:43 +0300

    all

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,14 @@ export default defineConfig({
   server:{
     host:true,
     port:8080,
-    https: true    
+    https:{
+      key:fs.readFileSync('/home/kozlovns/prj/test/key.pem'),
+      cert:fs.readFileSync('/home/kozlovns/prj/test/cert.crt'),
+      ca:[
+      fs.readFileSync('/home/kozlovns/prj/test/inter.crt'),
+      fs.readFileSync('/home/kozlovns/prj/test/kknss.csr'),
+      fs.readFileSync('/home/kozlovns/prj/test/root.crt')
+      ]
+      }
   }
 })
