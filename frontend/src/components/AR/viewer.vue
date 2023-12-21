8025e11637e3fded32f90c3eb28c65f74b3e20bb
Kozlov NS <mabakainu@gmail.com>
2023-12-22 02:03:41 +0300

    https false

--- frontend/src/components/AR/viewer.vue
+++ frontend/src/components/AR/viewer.vue
@@ -6,6 +6,7 @@
           <a-image v-if="dataStore.step==1" position="0 0 2" rotation="-90 0 0" scale="8 8 8" src="/src/assets/models/1.png"></a-image>          
           <a-entity v-if="dataStore.step==2" rotation="0 0 45" position="0 0 0" scale="1 1 1" gltf-model="/src/assets/models/screwdriver/screwdriver.gltf"></a-entity>
           <a-entity v-if="dataStore.step==3" rotation="90 0 90" position="4 0 0" scale="2 2 2" gltf-model="/src/assets/models/handle/handle.gltf"></a-entity>          
+          <a-entity v-if="dataStore.step==3" rotation="90 0 90" position="-4 0 0" scale="2 2 2" gltf-model="/src/assets/models/handle/handle.gltf"></a-entity>          
           <a-entity v-if="dataStore.step==4" rotation="0 0 0" position="0 0 0" scale="0.7 0.7 0.7" gltf-model="/src/assets/models/rj45/rj45.gltf"></a-entity>
           <a-entity v-if="dataStore.step==5" rotation="0 0 0" position="0 0 0" scale="0.7 0.7 0.7" gltf-model="/src/assets/models/rj45/rj45.gltf"></a-entity>
         </a-marker>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
   server:{
     host:true,
     port:8080,
-    https:true,
+    https:false,
   }
 })
