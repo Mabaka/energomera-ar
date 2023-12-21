f1d3941dd4a739d69d813096a49dc9de10aaf07c
Kozlov NS <mabakainu@gmail.com>
2023-12-22 01:30:21 +0300

    https

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import fs from 'fs'
 import mkcert from 'vite-plugin-mkcert'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),mkcert()],
   resolve:{
     alias:{
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -14,14 +14,6 @@ export default defineConfig({
   server:{
     host:true,
     port:8080,
-    https:{
-      key:fs.readFileSync('/etc/ssl/private/kknss-key.pem'),
-      cert:fs.readFileSync('/etc/ssl/certs/kknss-cert.crt'),
-      ca:[
-          fs.readFileSync('/etc/ssl/mdl/kknss-mdl.crt'),
-          fs.readFileSync('/etc/ssl/req/kknss-req.csr'),
-          fs.readFileSync('/etc/ssl/root/kknss-root.crt')
-      ]
-  }
+    https:true,
   }
 })
