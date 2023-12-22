7c6f48614cb2d43ce5b76be3f082d316063cf5a2
Nikita <mabakainu@gmail.com>
2023-12-22 16:50:08 +0300

    vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from "url";
+import fs from 'fs'
 import mkcert from 'vite-plugin-mkcert'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),mkcert()],
+  plugins: [vue()],
   resolve:{
     alias:{
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -13,6 +14,14 @@ export default defineConfig({
   server:{
     host:true,
     port:8080,
-    https:false,
+    https:{
+      key:fs.readFileSync('/etc/ssl/private/kknss-key.pem'),
+      cert:fs.readFileSync('/etc/ssl/certs/kknss-cert.crt'),
+      ca:[
+          fs.readFileSync('/etc/ssl/mdl/kknss-mdl.crt'),
+          fs.readFileSync('/etc/ssl/req/kknss-req.csr'),
+          fs.readFileSync('/etc/ssl/root/kknss-root.crt')
+      ]
+  }
   }
 })
